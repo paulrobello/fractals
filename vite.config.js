b8eda7a731778ce0d1650e0a0666174bdcd796db
Paul Robello <probello@gmail.com>
2025-10-25 16:36:55 -0700

    chore(config): add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
+  base: '/fractals/',
   plugins: [
     glsl({
       include: ['**/*.glsl', '**/*.vert', '**/*.frag', '**/*.vs', '**/*.fs'],
